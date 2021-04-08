6ff11c54035476cbe2a91feba672f2ca619a4d7f
Anzor <48889137+anzorayubov@users.noreply.github.com>
2021-04-08 10:27:22 +0300

    chart for tech params: add translate func

--- chart for tech params TEST/script.js
+++ chart for tech params TEST/script.js
@@ -11,6 +11,8 @@ self.onDataUpdated = function () {
     self.ctx.flot.update()
 
     showLastValuesInLegend()
+
+    translateDate()
 }
 
 const selectedParams = JSON.parse(sessionStorage.getItem('selected_tech_params')) || []
@@ -60,7 +62,7 @@ function addCheckboxes() {
         }
     }, 100))
 
-    $('.tb-legend-keys td:first-child input').click(event => {
+    checkboxes.click(event => {
         const label = event.target.closest('tr').querySelector('.tb-legend-label')
         label.dispatchEvent(clickEvent)
 
@@ -128,6 +130,82 @@ function debounce(fn, wait) {
         }
         clearTimeout(timeout)
         timeout = setTimeout(later, wait)
+    }
+}
+
+function translateDate() {
+    const locale = getLocale()
+    const dates = self.ctx.$container[0].querySelectorAll('.flot-x-axis div')
+    const russianWords = /[а-яё]/i;
+    const isOnlyNumbers = str => /^\d+$/.test(
+        str.replace(/[^a-zа-яё0-9\s]/gi, ' ').replace(/\s/g, ''))
+
+    Array.from(dates).forEach(date => {
+        const value = date.innerHTML
+        const number = isOnlyNumbers(value) ? value : value.slice(value.indexOf(" "))
+
+        if (!russianWords.test(value)) {
+            const month = value.slice(0, value.indexOf(" "))
+            date.innerHTML = `${locale[month] || ''} ${number}`
+        }
+    })
+}
+
+function getLocale() {
+    return {
+        "Sun": "Вс",
+        "Mon": "Пн",
+        "Tue": "Вт",
+        "Wed": "Ср",
+        "Thu": "Чт",
+        "Fri": "Пт",
+        "Sat": "Сб",
+        "Jan": "Янв.",
+        "Feb": "Февр.",
+        "Mar": "Март",
+        "Apr": "Апр.",
+        "May": "Май",
+        "Jun": "Июнь",
+        "Jul": "Июль",
+        "Aug": "Авг.",
+        "Sep": "Сент.",
+        "Oct": "Окт.",
+        "Nov": "Нояб.",
+        "Dec": "Дек.",
+        "January": "Январь",
+        "February": "Февраль",
+        "March": "Март",
+        "April": "Апрель",
+        "June": "Июнь",
+        "July": "Июль",
+        "August": "Август",
+        "September": "Сентябрь",
+        "October": "Октября",
+        "November": "Ноябрь",
+        "December": "Декабрь",
+        "Custom Date Range": "Пользовательский диапазон дат",
+        "Date Range Template": "Шаблон диапазона дат",
+        "Today": "Сегодня",
+        "Yesterday": "Вчера",
+        "This Week": "На этой неделе",
+        "Last Week": "Прошлая неделя",
+        "This Month": "Этот месяц",
+        "Last Month": "Прошлый месяц",
+        "Year": "Год",
+        "This Year": "В этом году",
+        "Last Year": "Прошлый год",
+        "Date picker": "Выбор даты",
+        "Hour": "Час",
+        "Day": "День",
+        "Week": "Неделю",
+        "2 weeks": "2 Недели",
+        "Month": "Месяц",
+        "3 months": "3 Месяца",
+        "6 months": "6 Месяцев",
+        "Custom interval": "Пользовательский интервал",
+        "Interval": "Интервал",
+        "Step size": "Размер шага",
+        "Ok": "Ok"
     }
 }
 
